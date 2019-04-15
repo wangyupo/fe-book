dcc4dacea3bec7244132b81cc5f65ceac4f82cb3
wangyupo <1106774262@qq.com>
2019-04-15 17:05:20 +0800

    feat: vue-press

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "正在切换分支..."
+git checkout vuePress
 echo "正在添加文件..."
 git add .
 echo -n "正在提交备注...，请填写备注（可空）"
@@ -10,4 +12,6 @@ fi
 git commit -m "$remarks"
 echo "正在开始提交代码..."
 git push origin vuePress
-echo "代码提交成功，正在关闭..."
+echo "代码提交成功，正在切回master分支..."
+git checkout master
+echo "已切回master分支，正在关闭..."
