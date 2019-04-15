cbe8e076780eb242083ec59c6a01ab7b96a011b5
wangyupo <1106774262@qq.com>
2019-04-15 18:11:02 +0800

    feat: 更新vuePress

--- push.sh
+++ push.sh
@@ -17,17 +17,17 @@ git init
 git add -A
 git commit -m 'deploy' >&-
 git remote add origin https://github.com/wangyupo/fe-book.git
-git push -f origin master
+git push -f origin master >&-
 
 cd -
 
 printf -- '\n\033[34m 静态页面推送完毕，开始更新vuePress... \033[0m\n\n';
 
 git add .
-git commit -m 'feat: 更新vuePress'
+git commit -m 'feat: 更新vuePress' >&-
 git checkout vuePress
-git merge master
-git push origin vuePress
+git merge master >&-
+git push origin vuePress >&-
 git checkout master
 
 printf -- '\n\033[32m SUCCESS: vuePress更新完毕，正在关闭... \033[0m\n\n';
