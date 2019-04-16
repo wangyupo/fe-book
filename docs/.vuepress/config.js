5b1fafb560d32513f2425d03c578e566ac1f328f
wangyupo <1106774262@qq.com>
2019-04-16 17:40:19 +0800

    feat: 更新vuePress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
     themeConfig: {
         nav: [
             { text: 'js', link: '/doc-js/数组/' },
+            { text: 'ui', link: '/doc-ui/UI设计规范/' },
             { text: '博客', link: 'https://wangyupo.github.io/' }
         ],
         sidebar: {
@@ -21,6 +22,37 @@ module.exports = {
                         '/doc-js/数组/树状数组递归删除.md',
                         '/doc-js/数组/数组排序.md',
                         '/doc-js/数组/数组去重.md',
+                    ]
+                },
+            ],
+            '/doc-ui/': [
+                {
+                    title: 'UI设计规范',
+                    path: '/doc-ui/UI设计规范/',
+                    children: [
+                        '/doc-ui/UI设计规范/为什么要有web页面设计规范？.md',
+                        '/doc-ui/UI设计规范/web页面设计规范包含哪些方面？.md',
+                        '/doc-ui/UI设计规范/网页设计中常见的字体规范.md',
+                        '/doc-ui/UI设计规范/行间距及段间距规范.md',
+                        '/doc-ui/UI设计规范/文字排版四原则.md',
+                        '/doc-ui/UI设计规范/web页中常见布局方式.md',
+                        '/doc-ui/UI设计规范/栅格化系统.md',
+                        '/doc-ui/UI设计规范/内容间隔.md',
+                        '/doc-ui/UI设计规范/常用行高.md',
+                        '/doc-ui/UI设计规范/常用边角.md',
+                        '/doc-ui/UI设计规范/常用阴影.md',
+                        '/doc-ui/UI设计规范/常用字号.md',
+                    ]
+                },
+                {
+                    title: 'UI知识点',
+                    path: '/doc-ui/UI知识点/',
+                    children: [
+                        '/doc-ui/UI知识点/HSB色彩模型.md',
+                        '/doc-ui/UI知识点/UI基于对接的工作.md',
+                        '/doc-ui/UI知识点/网站常见的设计形式.md',
+                        '/doc-ui/UI知识点/网页的配色方案如何确定？有哪些方法？.md',
+                        '/doc-ui/UI知识点/导航栏高度.md',
                     ]
                 },
             ],
